4d2e5b6291c99927d69d94d990920ce9e28ac0c4
Gabriel Blanca Montes <how2hack@hotmail.es>
2020-02-03 21:52:07 +0100

    método en una línea

--- geometria/src/Triangulo_GBM.java
+++ geometria/src/Triangulo_GBM.java
@@ -23,12 +23,7 @@ public class Triangulo_GBM extends FiguraGeometrica_GBM
     public double area()
     { // Utiliza la formula de Her�n
         double sp;
-        sp = this.semiPerimetro();
+        sp = this.perimetro() / 2;
         return Math.sqrt(sp * (sp - lado1) * (sp - lado2) * (sp - lado3));
-    }
-    
-    public double semiPerimetro()
-    {
-        return this.perimetro() / 2;
     }
 }
