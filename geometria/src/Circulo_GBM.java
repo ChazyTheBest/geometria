c588c03ceb865f0bb83ee9bab509f9ab07f2da13
Gabriel Blanca Montes <how2hack@hotmail.es>
2020-02-03 22:17:12 +0100

    Siempre positivo

--- geometria/src/Circulo_GBM.java
+++ geometria/src/Circulo_GBM.java
@@ -4,24 +4,23 @@ public class Circulo_GBM extends FiguraGeometrica_GBM
     private double radio;
     // Constante pi
     static final double PI = 3.1416;
-    
+
     public Circulo_GBM(double r, String tipoFigura)
     {
         super(tipoFigura);
-        radio = r;
+        radio = r < 0 ? r * (-1) : r;
     }
-    
+
     @Override
     public double area()
     {
         return radio * radio * PI;
-        
     }
-    
+
     @Override
     public double perimetro()
     {
         return radio * 2 * PI;
     }
-    
+
 }
